524801dd70fd12f087247acbb8e2fa90f720cc58
ĐŘΔǤ <147419071+despair-dragon@users.noreply.github.com>
2023-11-01 21:02:16 +0300

    Add files via upload

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,10 +8,6 @@ export default function App() {
         document.getElementById('app').style.height = `${height}px`
     }, );
 
-
-
-
-
     return (
         <div className="app" id='app'>
             <div className='app-center'>
@@ -22,7 +18,7 @@ export default function App() {
                     </div>
                     <div className='btn-container'>
                         <div className='btn-container-line'>
-                            <div id='btn' className='btn btn-primary'>
+                            <div className='btn btn-primary'>
                                 <svg style={{marginRight: '125px'}} className="btn-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16">
                                     <path fillRule="evenodd" d="M7.47 10.78a.75.75 0 001.06 0l3.75-3.75a.75.75 0 00-1.06-1.06L8.75 8.44V1.75a.75.75 0 00-1.5 0v6.69L4.78 5.97a.75.75 0 00-1.06 1.06l3.75 3.75zM3.75 13a.75.75 0 000 1.5h8.5a.75.75 0 000-1.5h-8.5z"></path>
                                 </svg>
--- src/hook/getClientHeight.jsx
+++ src/hook/getClientHeight.jsx
@@ -1,71 +1,12 @@
-<div align="center">
-    <h1>
-        <a>Day</a>
-        <a href="-" target="_blank">1</a>
-    </h1>
-    <h3>
-        <div>
-            Oddly enough, I'm <a href="-" target="_blank">studying</a> in college and now I'm going there
-        </div>
-        <div>
-            I think <a href="-" target="_blank">today</a> I will make a plan and tell you what I want to achieve
-        </div>
-    </h3>
-    <h3 align="left"> ⠀⠀⠀⠀⠀⠀⠀⠀⠀
-        <a href="-" target="_blank">-</a> Skills I want to <a href="-" target="_blank">learn</a>
-    </h3>
-    <h4 align="left">
-        <div> ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-            <image src="https://github.com/Devollox/My-way/blob/main/image/sass.png" height="12px"><a href="-"
-                                                                                                      target="_blank">SASS</a>
-        </div>
-        <div> ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-            <a href="-" target="_blank">
-                <image src="https://github.com/Devollox/My-way/blob/main/image/git.png" height="12px"> Git
-            </a>
-        </div>
-        <div> ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-            <image src="https://github.com/Devollox/My-way/blob/main/image/JS.png" height="12px"> Java<a href="-"
-                                                                                                         target="_blank">Script</a>
-        </div>
-        <div> ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-            <image src="https://github.com/Devollox/My-way/blob/main/image/TS.png" height="12px"> Type<a href="-"
-                                                                                                         target="_blank">Script</a>
-        </div>
-        <div> ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-            <image src="https://github.com/Devollox/My-way/blob/main/image/react.png" height="12px"><a href="-"
-                                                                                                       target="_blank">React</a>
-        </div>
-        <div> ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-            <image src="https://github.com/Devollox/My-way/blob/main/image/tail.png" height="12px"><a href="-"
-                                                                                                      target="_blank">Tailwind</a>
-        </div>
-        <div> ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-            <image src="https://github.com/Devollox/My-way/blob/main/image/rx.png" height="12px"> Rx<a href="-"
-                                                                                                       target="_blank">JS</a>
-        </div>
-        <div> ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-            <image src="https://github.com/Devollox/My-way/blob/main/image/5848309bcef1014c0b5e4a9a.png" height="12px">
-                <a href="-" target="_blank">Redux</a>
-        </div>
-        <div> ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-            <image src="https://github.com/Devollox/My-way/blob/main/image/Node.png" height="12px"> Node.<a href="-"
-                                                                                                            target="_blank">Js</a>
-        </div>
-    </h4>
-    <h4>
-        <a>
-            I'm <a href="-" target="_blank">starting</a> not only to learn <a href="-" target="_blank">programming</a>,
-            but also <a href="-" target="_blank">physics</a>, so there will be thoughts not only about the <a href="-"
-                                                                                                              target="_blank">code</a>
-        </a>
-        <div>
-            I have a blank about the <a href="-" target="_blank">roadmap</a>, so <a href="-" target="_blank">
-            I'm</a> thinking of making it and uploading it to my <a href="-" target="_blank">site</a>, there to make <a
-            href="-" target="_blank">modal windows</a>
-        </div>
-    </h4>
-    <h3>
-        <a href="https://devollox.fun/" target="_blank">Site</a> - Here you can see
-    </h3>
-</div>
+/* eslint-disable */
+
+import {useEffect} from "react";
+
+export default function getClientHeight() {
+    useEffect(() => {
+        let height = document.documentElement.clientHeight
+        document.getElementById('app').style.height = `${height}px`
+    }, );
+}
+
+// Poor optimization
